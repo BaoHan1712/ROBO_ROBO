56833faa850c914f88532a2fac9e6ee534a68791
HanBao <154613476+BaoHan1712@users.noreply.github.com>
2024-10-07 15:33:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import cv2
 from ultralytics import YOLO
 import numpy as np
-from utils import  offset_backboard
+from utils import  process_distance, calculator_offset
 
-cap = cv2.VideoCapture("data\clip_7d.mp4")
+cap = cv2.VideoCapture("data\o.MOV")
 
 class_name = ['backboard', 'ball', 'basket']
+model = YOLO("model\\yolo11.onnx")
 
-model = YOLO("model\yolo11.onnx")
+################# Sửa lại thông số trong này  ##############
+KNOWN_DISTANCE_BACKBOARD = 50  # Khoảng cách từ camera tới backboard
+KNOWN_HEIGHT_BACKBOARD = 70  # Chiều cao của backboard 
 
-#fps
+KNOWN_DISTANCE_BASKET = 40  # Khoảng cách từ camera tới backboard
+KNOWN_HEIGHT_BASKET = 50  # Chiều cao của backboard 
+
+
 prev_frame_time = 0
 new_frame_time = 0
 
@@ -19,41 +25,43 @@ while True:
         break
 
     new_frame_time = cv2.getTickCount()
-
     frame = cv2.resize(frame, (1080, 720))
-    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # contour(frame,hsv)
 
     results = model.predict(source=frame, imgsz=320, conf=0.65)
 
     for result in results:
-        boxes = result.boxes 
+        boxes = result.boxes
         for box in boxes:
-            x1, y1, x2, y2 = map(int, box.xyxy[0])  
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
             cls = int(box.cls[0])
 
-            if cls == 0 or cls == 2 or cls == 1:
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
+            if cls == 0 :  
                 label = f'{class_name[cls]}'
-                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-            if cls == 0 :
-                w = x2 - x1
-                h = y2 - y1
-                cx, cy = x1 + w // 2, y1 + h // 2
+                process_distance(frame, x1, y1, y2,  KNOWN_HEIGHT_BACKBOARD,KNOWN_DISTANCE_BACKBOARD)
 
-                # Vẽ đường tròn ở giữa bbox
+            # Vẽ đường tròn tại trung tâm
+                cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
                 cv2.circle(frame, (cx, cy), 6, (0, 0, 255), -1)
 
-                #Gọi hàm offset
-                offset = offset_backboard(frame,cx)
+                calculator_offset(frame , cx , x1 , y2)
 
-                # In độ lệch so với trục trung tâm
-                if offset < -2:
-                    cv2.putText(frame, f'Lech trai: {abs(offset)} px', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
-                elif offset > 2:
-                    cv2.putText(frame, f'Lech phai: {abs(offset)} px', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+            elif cls == 2 :  
+                label = f'{class_name[cls]}'
+
+                process_distance(frame, x1, y1, y2,  KNOWN_HEIGHT_BASKET,KNOWN_DISTANCE_BASKET)
+
+            # Vẽ đường tròn tại trung tâm
+                cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+                cv2.circle(frame, (cx, cy), 6, (0, 0, 255), -1)
+
+                calculator_offset(frame , cx, x1 , y2 )
+
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     fps = cv2.getTickFrequency() / (new_frame_time - prev_frame_time)
     prev_frame_time = new_frame_time
